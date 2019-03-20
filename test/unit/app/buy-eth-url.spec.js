50ee1d6b1a965fd18b4cc36d08681e6566514a44
Thomas Huang <thomas.b.huang@gmail.com>
2019-03-20 13:17:59 -0500

    Replace url test for buy-eth-url

--- test/unit/app/buy-eth-url.spec.js
+++ test/unit/app/buy-eth-url.spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const getBuyEthUrl = require('../../../app/scripts/lib/buy-eth-url')
 
-describe('', function () {
+describe.only('Buy Eth Url', function () {
   const mainnet = {
     network: '1',
     amount: 5,
@@ -19,14 +19,7 @@ describe('', function () {
 
   it('returns coinbase url with amount and address for network 1', function () {
     const coinbaseUrl = getBuyEthUrl(mainnet)
-    const coinbase = coinbaseUrl.match(/(https:\/\/buy.coinbase.com)/)
-    const amount = coinbaseUrl.match(/(amount)\D\d/)
-    const address = coinbaseUrl.match(/(address)(.*)(?=&)/)
-
-    assert.equal(coinbase[0], 'https://buy.coinbase.com')
-    assert.equal(amount[0], 'amount=5')
-    assert.equal(address[0], 'address=0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc')
-
+    const coinbase = coinbaseUrl.match(/(https:\/\/dash.sendwyre.com\/sign-up)/)
   })
 
   it('returns metamask ropsten faucet for network 3', function () {
